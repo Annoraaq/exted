26bdfe3e304a8ecf92064621363f9e1b147dc315
Johannes Baum <baum@bw-webgestaltung.de>
2018-01-28 00:33:47 +0100

    Make publishable on hex

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule ExTed.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
     ]
   end
 
@@ -18,9 +20,23 @@ defmodule ExTed.MixProject do
     ]
   end
 
+  defp description() do
+    "ExTed is a naive implementation of a tree edit distance algorithm in Elixir. Please note that this algorithm has exponential runtime complexity!"
+  end
+  
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Johannes Baum"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/Annoraaq/exted"}
+    ]
+  end
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
